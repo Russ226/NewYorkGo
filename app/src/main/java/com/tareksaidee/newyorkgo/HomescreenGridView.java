ad5f31fd49414efb3390bd764a2a2a83728184c1
Tarek <tareksaidee@hotmail.com>
2018-04-27 23:22:25 -0400

    change name

--- app/src/main/java/com/tareksaidee/newyorkgo/HomescreenGridView.java
+++ app/src/main/java/com/tareksaidee/newyorkgo/HomescreenGridView.java
@@ -1,22 +1,23 @@
 package com.tareksaidee.newyorkgo;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
-public class HomescreenGridView {
+public class HomescreenGridView extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        //gridview.setAdapter(new ImageAdapter(this));
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Toast.makeText(HomescreenGridView.this, "" + position, Toast.LENGTH_SHORT).show();
             }
-        }
+        });
     }
 }
