5320a5a8c5dbd93352f85a9ab6d0cd3eba331c32
Tarek <tareksaidee@hotmail.com>
2018-04-28 03:42:31 -0400

    add many things

--- app/src/main/java/com/tareksaidee/newyorkgo/bbq/BBQActivity.java
+++ app/src/main/java/com/tareksaidee/newyorkgo/bbq/BBQActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.tareksaidee.newyorkgo.DTO.BBQ;
 import com.tareksaidee.newyorkgo.R;
+import com.tareksaidee.newyorkgo.parser.JsonParser;
 
 import java.util.ArrayList;
 
@@ -15,11 +17,19 @@ public class BBQActivity extends AppCompatActivity {
     private ArrayList<BBQ> bbqs;
     private RecyclerView bbqsView;
     private BBQAdapter bbqAdapter;
+    private JsonParser parser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bbq);
+        parser = new JsonParser();
+        try {
+            bbqs = parser.getBBQ(getAssets());
+        } catch (Exception e) {
+            Log.e("BBQ", e.getMessage());
+        }
         bbqsView = findViewById(R.id.bbqRecyclerView);
         bbqAdapter = new BBQAdapter(this, bbqs);
         bbqsView.setAdapter(bbqAdapter);
