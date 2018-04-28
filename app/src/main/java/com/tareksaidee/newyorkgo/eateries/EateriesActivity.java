be13aba6f44d91c80f7fade0e5e9f2719f09e7b3
jyang001 <joeyang095@gmail.com>
2018-04-28 17:25:06 -0400

    add EateriesActivity Adapter and xml (check eateries json)

---
+++ app/src/main/java/com/tareksaidee/newyorkgo/eateries/EateriesActivity.java
@@ -0,0 +1,39 @@
+package com.tareksaidee.newyorkgo.eateries;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.tareksaidee.newyorkgo.DTO.Eateries;
+import com.tareksaidee.newyorkgo.eateries.EateriesAdapter;
+import com.tareksaidee.newyorkgo.parser.JsonParser;
+
+import java.util.ArrayList;
+
+public class EateriesActivity extends AppCompatActivity {
+
+    private ArrayList<Eateries> E;
+    private RecyclerView eateriesView;
+    private EateriesAdapter eateriesAdapter;
+    private JsonParser parser;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eateries);
+        parser = new JsonParser();
+        try {
+            E = parser.getEateries(getAssets());
+        } catch (Exception e) {
+            Log.e("Eateries", e.getMessage());
+        }
+        eateriesView = findViewById(R.id.eateriesRecyclerView);
+        eateriesAdapter = new EateriesAdapter(this, E);
+        eateriesView.setAdapter(eateriesAdapter);
+        eateriesView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+    }
+}
---
+++ app/src/main/java/com/tareksaidee/newyorkgo/eateries/EateriesAdapter.java
@@ -0,0 +1,74 @@
+package com.tareksaidee.newyorkgo.eateries;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.tareksaidee.newyorkgo.DTO.Eateries;
+
+import java.util.ArrayList;
+
+public class EateriesAdapter extends RecyclerView.Adapter<EateriesAdapter.EateriesViewHolder> {
+
+    ArrayList<Eateries> e;
+    private Context mContext;
+
+    EateriesAdapter(@NonNull Context context, ArrayList<Eateries> e) {
+        mContext = context;
+        this.e = e;
+
+    }
+
+    @Override
+    public EateriesAdapter.EateriesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.eateries_card, parent, false);
+        return new EateriesAdapter.EateriesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EateriesAdapter.EateriesViewHolder holder, int position) {
+        Eateries eatery = e.get(position);
+        holder.location.setText(eatery.getLocation());
+        holder.name.setText(eatery.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return e.size();
+    }
+
+    void clear() {
+        e.clear();
+    }
+
+    class EateriesViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name;
+        TextView location;
+        TextView parkID;
+        TextView startDate;
+        TextView endDate;
+        TextView description;
+        TextView permitNumber;
+        TextView phone;
+        TextView website;
+        TextView typeName;
+
+        EateriesViewHolder(View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.name);
+            parkID = (TextView) view.findViewById(R.id.location);
+            startDate = (TextView) view.findViewById(R.id.startDate);
+            endDate = (TextView) view.findViewById(R.id.endDate);
+            description = (TextView) view.findViewById(R.id.description);
+            permitNumber = (TextView) view.findViewById(R.id.permitNumber);
+            phone = (TextView) view.findViewById(R.id.phone);
+            website = (TextView) view.findViewById(R.id.website);
+            typeName = (TextView) view.findViewById(R.id.typeName);
+        }
+    }
+}
