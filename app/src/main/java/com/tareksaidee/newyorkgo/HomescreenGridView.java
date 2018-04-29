060effc7820c056c1c944b613b71715ccef502e4
Steven Woo <swoo328@yahoo.com>
2018-04-29 10:46:19 -0400

    update homepage

--- app/src/main/java/com/tareksaidee/newyorkgo/HomescreenGridView.java
+++ app/src/main/java/com/tareksaidee/newyorkgo/HomescreenGridView.java
@@ -6,10 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
 
 import com.tareksaidee.newyorkgo.DTO.Category;
+import com.tareksaidee.newyorkgo.DTO.IndoorSwimmingPool;
 import com.tareksaidee.newyorkgo.artgallery.ArtGalleryActivity;
 import com.tareksaidee.newyorkgo.bball.BasketballCourtActivity;
 import com.tareksaidee.newyorkgo.bbq.BBQActivity;
 import com.tareksaidee.newyorkgo.beaches.BeachesActivity;
+import com.tareksaidee.newyorkgo.dogruns.DogRunsActivity;
+import com.tareksaidee.newyorkgo.eateries.EateriesActivity;
+import com.tareksaidee.newyorkgo.handball.HandballActivity;
+import com.tareksaidee.newyorkgo.hiking.HikingActivity;
+import com.tareksaidee.newyorkgo.iceskating.IceSkatingActivity;
+import com.tareksaidee.newyorkgo.indoorswimmingpool.IndoorSwimmingPoolActivity;
+import com.tareksaidee.newyorkgo.naturepreserves.NaturePreservesActivity;
+import com.tareksaidee.newyorkgo.outdoorswimming.OutdoorSwimmingActivity;
+import com.tareksaidee.newyorkgo.park.ParkActivity;
+import com.tareksaidee.newyorkgo.tennis.TennisActivity;
+import com.tareksaidee.newyorkgo.theater.TheaterActivity;
+import com.tareksaidee.newyorkgo.zooaq.ZooAqActivity;
 
 import java.util.ArrayList;
 
@@ -36,26 +49,30 @@ public class HomescreenGridView extends AppCompatActivity {
         categories.add(category);
         category = new Category("Beaches", ContextCompat.getDrawable(this, R.drawable.beaches), BeachesActivity.class);
         categories.add(category);
-        /*category = new Category("Dogs", ContextCompat.getDrawable(this, R.drawable.dogs), BeachesActivity.class);
-        categories.add(category);*/
-
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-//        category = new Category("Art Gallery", ContextCompat.getDrawable(this, R.drawable.ic_launcher_background), ArtGalleryActivity.class);
-//        categories.add(category);
-
+        category = new Category("Dog Runs", ContextCompat.getDrawable(this, R.drawable.dogs), DogRunsActivity.class);
+        categories.add(category);
+        category = new Category("Eateries", ContextCompat.getDrawable(this, R.drawable.eateries), EateriesActivity.class);
+        categories.add(category);
+        category = new Category("Handball", ContextCompat.getDrawable(this, R.drawable.handball_courts), HandballActivity.class);
+        categories.add(category);
+        category = new Category("Hiking", ContextCompat.getDrawable(this, R.drawable.hiking), HikingActivity.class);
+        categories.add(category);
+        category = new Category("Ice Skating", ContextCompat.getDrawable(this, R.drawable.iceskating), IceSkatingActivity.class);
+        categories.add(category);
+        category = new Category("Indoor Swimming Pool", ContextCompat.getDrawable(this, R.drawable.indoorswimming), IndoorSwimmingPoolActivity.class);
+        categories.add(category);
+        category = new Category("Nature Preserves", ContextCompat.getDrawable(this, R.drawable.nature), NaturePreservesActivity.class);
+        categories.add(category);
+        category = new Category("Outdoor Swimming", ContextCompat.getDrawable(this, R.drawable.outdoorswimming),OutdoorSwimmingActivity.class);
+        categories.add(category);
+        category = new Category("Parks", ContextCompat.getDrawable(this, R.drawable.parks), ParkActivity.class);
+        categories.add(category);
+        category = new Category("Tennis Courts", ContextCompat.getDrawable(this, R.drawable.tenniscourts), TennisActivity.class);
+        categories.add(category);
+        category = new Category("Theaters", ContextCompat.getDrawable(this, R.drawable.theaters), TheaterActivity.class);
+        categories.add(category);
+        category = new Category("Zoo and Aquarium", ContextCompat.getDrawable(this, R.drawable.aquariums), ZooAqActivity.class);
+        categories.add(category);
     }
 
 
