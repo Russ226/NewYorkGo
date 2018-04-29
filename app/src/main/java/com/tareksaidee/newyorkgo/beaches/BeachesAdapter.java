6f5fafcda5dd90747e12c2e71dc8248a4bf99bb8
Tarek <tareksaidee@hotmail.com>
2018-04-29 10:22:55 -0400

    update breaches

--- app/src/main/java/com/tareksaidee/newyorkgo/beaches/BeachesAdapter.java
+++ app/src/main/java/com/tareksaidee/newyorkgo/beaches/BeachesAdapter.java
@@ -7,22 +7,37 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.tareksaidee.newyorkgo.DTO.Beaches;
+import com.tareksaidee.newyorkgo.DTO.Bookmark;
 import com.tareksaidee.newyorkgo.R;
 import com.tareksaidee.newyorkgo.ShowAddressActivity;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class BeachesAdapter extends RecyclerView.Adapter<BeachesAdapter.BeachesViewHolder> implements View.OnClickListener{
+public class BeachesAdapter extends RecyclerView.Adapter<BeachesAdapter.BeachesViewHolder>{
 
     ArrayList<Beaches> beaches;
     private Context mContext;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mBookmarksDatabaseReferenceFull;
+    private DatabaseReference mBookmarksDatabaseReferencePart;
+    private FirebaseAuth mFirebaseAuth;
 
     BeachesAdapter(@NonNull Context context, ArrayList<Beaches> beaches) {
         mContext = context;
         this.beaches = beaches;
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mBookmarksDatabaseReferenceFull = mFirebaseDatabase.getReference();
+        mBookmarksDatabaseReferencePart = mFirebaseDatabase.getReference();
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -33,7 +48,7 @@ public class BeachesAdapter extends RecyclerView.Adapter<BeachesAdapter.BeachesV
 
     @Override
     public void onBindViewHolder(BeachesAdapter.BeachesViewHolder holder, int position) {
-        Beaches beach = beaches.get(position);
+        final Beaches beach = beaches.get(position);
         //holder.location.setText(beach.getLocation());
         holder.name.setText(beach.getName());
         holder.phone.setText(beach.getPhone());
@@ -47,6 +62,30 @@ public class BeachesAdapter extends RecyclerView.Adapter<BeachesAdapter.BeachesV
         holder.boardWalk.setText(beach.getBoardwalk());
         holder.description.setText(beach.getDescription());
         holder.location.setText(beach.getLocation());
+        holder.bookmarkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mFirebaseAuth.getCurrentUser() != null) {
+                    String key = (new Random().nextInt(100000000)) + "";
+                    mBookmarksDatabaseReferenceFull.child(mFirebaseAuth.getCurrentUser().getUid() + "/full/" + key)
+                            .push().setValue(beach);
+                    Bookmark bookmark = new Bookmark(beach.getName(), key);
+                    mBookmarksDatabaseReferencePart.child(mFirebaseAuth.getCurrentUser().getUid() + "/part/")
+                            .push().setValue(bookmark);
+                    Toast.makeText(mContext, "Bookmarked", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, "Not logged in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        holder.mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, ShowAddressActivity.class);
+                intent.putExtra("address", beach.getLocation());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -71,6 +110,8 @@ public class BeachesAdapter extends RecyclerView.Adapter<BeachesAdapter.BeachesV
         TextView bikeSkate;
         TextView boardWalk;
         TextView description;
+        Button mapButton;
+        Button bookmarkButton;
 
         BeachesViewHolder(View view) {
             super(view);
@@ -86,13 +127,8 @@ public class BeachesAdapter extends RecyclerView.Adapter<BeachesAdapter.BeachesV
             bikeSkate = (TextView) view.findViewById(R.id.bikeAndSkate);
             boardWalk = (TextView) view.findViewById(R.id.boardwalk);
             description = (TextView) view.findViewById(R.id.description);
+            mapButton = (Button) view.findViewById(R.id.map);
+            bookmarkButton = (Button) view.findViewById(R.id.bookmark);
         }
-    }
-
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(mContext, ShowAddressActivity.class);
-        intent.putExtra("address", ((TextView) view.findViewById(R.id.location)).getText().toString());
-        mContext.startActivity(intent);
     }
 }
