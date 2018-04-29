23c1d0a96e38dfb949f9e1d70473f7a6db02bf26
Tarek <tareksaidee@hotmail.com>
2018-04-29 12:55:14 -0400

    Update buttons

---
+++ app/src/main/java/com/tareksaidee/newyorkgo/About.java
@@ -0,0 +1,16 @@
+package com.tareksaidee.newyorkgo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tarek on 4/29/2018.
+ */
+
+public class About extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_page);
+    }
+}
